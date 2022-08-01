fb3e401c1b7bd779f032d67f7d0a48ec5108c959
Bii <canaldesafios.1001@gmail.com>
2022-08-01 08:48:03 -0300

    função de verificar e de limpar o jogo

--- Nova pasta/js/script.js
+++ Nova pasta/js/script.js
@@ -13,3 +13,33 @@ function joga(celula){
         }
     }
 }
+
+function verifica(){
+    c11 = document.getElementById('cell11').innerHTML;
+    c12 = document.getElementById('cell12').innerHTML;
+    c13 = document.getElementById('cell13').innerHTML;
+    c21 = document.getElementById('cell21').innerHTML;
+    c22 = document.getElementById('cell22').innerHTML;
+    c23 = document.getElementById('cell23').innerHTML;
+    c31 = document.getElementById('cell31').innerHTML;
+    c32 = document.getElementById('cell32').innerHTML;
+    c33 = document.getElementById('cell33').innerHTML;
+    if (((c11 != '') && (c12 != '') && (c13 != '') &&
+    (c11 == c12) && (c12 == c13)) || ((c11 != '') &&
+    (c22 != '') && (c33 != '') && (c11 == c22) &&
+    (c22 == c33)) || ((c11 != '') && (c21 != '') &&
+    (c31 != '') && (c11 == c21) && (c21 == c31)) || ((c21 != '') &&
+    (c22 != '') && (c23 != '') && (c21 == c22) &&
+    (c22 == c23)) || ((c31 != '') && (c32 != '') && 
+    (c33 != '') && (c31 == c32) && (c32 == c33)) || (()) ))
+}
+
+// função para limpar o jogo
+function novo(){
+    for (i=1; i<4; i++){
+        for(j=1; j<4; i++){
+            nomecelula = 'cel' + i + j
+            document.getElementById(nomecelula).innerHTML = '';
+        }
+    }
+}
